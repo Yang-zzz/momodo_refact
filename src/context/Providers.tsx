e0458fe8d91a0465bc623511cfa837028f93bff1
Yangjisung <ygji6406@gmail.com>
2023-10-13 11:46:48 +0900

    fix : build error 작업 중

---
+++ src/context/Providers.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import React, { ReactNode, useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+interface Props {
+  children: ReactNode;
+}
+const queryClientOptions = {
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+};
+
+export const Providers = ({ children }: Props) => {
+  const [queryClient] = useState(() => new QueryClient(queryClientOptions));
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
